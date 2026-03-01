bc28cd75cf9349012abdc145935bf1cc01be6515
JamesBracken <james_bracken@yahoo.com>
2026-03-01 19:18:00 +0000

    Add: New ML files incorporating pickle instead of tensorflow

--- backend/mood/ml_service.py
+++ backend/mood/ml_service.py
@@ -11,11 +11,16 @@ BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ML_PATH = os.path.join(BASE, 'ml_model')
 
 # Load model and artifacts
-model = tf.keras.models.load_model(os.path.join(ML_PATH, 'nn_model.keras'))
+# model = tf.keras.models.load_model(os.path.join(ML_PATH, 'nn_model.pkl'))
 
-with open(os.path.join(ML_PATH, 'tfidf_vectorizer.pkl'), 'rb') as f:
-    tfidf = pickle.load(f)
+# with open(os.path.join(ML_PATH, 'tfidf_vectorizer.pkl'), 'rb') as f:
+#     tfidf = pickle.load(f)
 
-with open(os.path.join(ML_PATH, 'label_encoder.pkl'), 'rb') as f:
-    label_encoder = pickle.load(f)
+with open(os.path.join(ML_PATH, './nn_model.pkl'), 'rb') as f:
+    load_model = pickle.load(f)
+
+# with open(os.path.join(ML_PATH, 'label_encoder.pkl'), 'rb') as f:
+#     label_encoder = pickle.load(f)
     
+with open(os.path.join(ML_PATH,'label_encoder.pkl'), 'rb') as f: # If file in the root or provide the relative path
+    label_encoder = pickle.load(f)
