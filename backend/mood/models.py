0fe3795e3f9a095b550f8ca809604e42ccee4feb
JamesBracken <james_bracken@yahoo.com>
2026-03-01 09:23:54 +0000

    Add: Mood model, admin Register mood app

--- backend/mood/models.py
+++ backend/mood/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 from django.conf import settings
+from django.core.validators import MinLengthValidator
 
 class Mood(models.Model):
-    # id - dont make, Will be auto created by django
-    # user_mood_text
-    # user
-    # predicted_mood
-
     # CONSIDER ADDING AN OBJECT PROPERTY CONTAINING ALL MOOD CALCULATIONS 
     # INSTEAD OF JUST THE MAIN MOOD
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name="mood_predictions"
+        related_name="mood_predictions",
     )
-    # DOUBLE CHECK TEXT LIMIT WITH IGOR OR FRONTEND TEAM
-    user_mood_text = models.TextField(max_length=2000)
+    user_mood_text = models.TextField(validators=[MinLengthValidator(100)], max_length=2000)
     # LOOK AT RESTRICTING THIS FURTHER
-    predicted_mood = models.CharField
+    predicted_emotion = models.CharField(max_length=100)
+
+    # NEW FIELDS
+    date = models.DateField(auto_now_add=True)
+    sleep = models.CharField(null=True, blank=True, max_length=100)
+    gratitude= models.TextField(max_length=2000)
+    habits= models.JSONField(null=True)
