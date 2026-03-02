c46eedc0b071508314d2aa8331d48e237beda273
jaime.hyland@language-landscapes.com <james.hyland@iubh.de>
2026-03-02 09:10:40 +0100

    Switch to DEBUG=False programmatically.

--- backend/django_backend/settings.py
+++ backend/django_backend/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
